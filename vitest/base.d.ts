2808ba5e524ad81bcedb121d1c679757bbeacaa2
CapnQwan <q.p1453@gmail.com>
2026-09-13 12:18:28 +1200

    added the pre-commit and pre-push hooks to prevent a bunch of minor changes from being sequenced from heaps of minor errors

--- vitest/base.d.ts
+++ vitest/base.d.ts
@@ -1,17 +1,15 @@
 export interface BaseVitestOptions {
-    aliasName?: string;
-    aliasEntry?: string;
-    cwd?: string;
-    testOverrides?: Record<string, unknown>;
-  }
-  
-  export interface BaseVitestConfig {
-    test: Record<string, unknown>;
-    resolve: {
-      alias?: Record<string, string>;
-    };
-  }
-  
-  export declare function baseVitestConfig(
-    options?: BaseVitestOptions
-  ): BaseVitestConfig;
+  aliasName?: string;
+  aliasEntry?: string;
+  cwd?: string;
+  testOverrides?: Record<string, unknown>;
+}
+
+export interface BaseVitestConfig {
+  test: Record<string, unknown>;
+  resolve: {
+    alias?: Record<string, string>;
+  };
+}
+
+export declare function baseVitestConfig(options?: BaseVitestOptions): BaseVitestConfig;
